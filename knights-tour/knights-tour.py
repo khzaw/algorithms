a933ed7e526e0bc6ccddd7fb7e1d43748832e270
Kaung Htet Zaw <emoosx@gmail.com>
2016-02-12 17:54:14 +0800

    Knight's tour using Warnsdorff heuristics

--- knights-tour/knights-tour.py
+++ knights-tour/knights-tour.py
@@ -9,7 +9,7 @@ def chess2index(p, boardsize):
 def is_legal(pos, board, boardsize):
     return 0 <= (pos[X]) < boardsize[X] and \
            0 <= (pos[Y]) < boardsize[Y] and \
-           board[pos] == 0
+           not board[pos]
 
 def board_string(board, boardsize):
     string = ''
@@ -18,6 +18,20 @@ def board_string(board, boardsize):
             string += '%3d' % board[(x, y)]
         string += '\n'
     return string
+
+def next_possible_moves(current, board, boardsize):
+    next_moves = [(current[X]+d[X], current[Y]+d[Y]) for d in offsets]
+    next_moves = [p for p in next_moves if is_legal(p, board, boardsize)]
+    return next_moves
+
+def warnsdorff(current, board, boardsize):
+    moves = []
+    for move in next_possible_moves(current, board, boardsize):
+        temp = board[move]
+        board[move] = -1
+        moves.append((move, len(next_possible_moves(move, board, boardsize))))
+        board[move] = temp
+    return sorted(moves, key=lambda x:x[1])
 
 def knight_tour(currentpos, board, count):
     if count >= boardsize[X] * boardsize[Y]:
@@ -39,15 +53,28 @@ def knight_tour(currentpos, board, count):
 
 
 if __name__ == '__main__':
-    n = int(raw_input('n: '))
+    n = int(raw_input('N: '))
     boardsize = (n, n)
     start = chess2index(raw_input('Start: '), boardsize)
     offsets = ((1, 2), (-1, 2), (1, -2), (-1, -2),
-            (2, 1), (-2, 1), (2, -1), (-2, -1))
+               (2, 1), (-2, 1), (2, -1), (-2, -1))
     board = {(x, y):0 for x in range(boardsize[X]) for y in range(boardsize[Y])}
     count = 1
     board[start] = count
-    if knight_tour(start, board, count):
-        print board_string(board, boardsize)
-    else:
-        print "No knight's tour"
+
+    current = start
+    while count < len(board):
+        p = warnsdorff(current, board, boardsize)
+        p = p[0][0]
+        count += 1
+        board[p] = count
+        current = p
+
+    print "Done!"
+    print board_string(board, boardsize)
+
+
+    #  if knight_tour(start, board, count):
+        #  print board_string(board, boardsize)
+    #  else:
+        #  print "No knight's tour"
