4a03817594db7ca65c2bf04606235298ebdfb092
Kaung Htet Zaw <emoosx@gmail.com>
2016-03-19 00:13:02 +0800

    Add breadth first search and depth first search

---
+++ graph-search/bfs.py
@@ -0,0 +1,38 @@
+from node import Node
+from collections import deque
+
+def breadth_first_search(root, search_value):
+    q = deque([root])
+    root.visited = True
+
+    while len(q) > 0:
+        node = q.pop()
+        for neighbour in node.adjacentNodes:
+            if not neighbour.visited:
+                q.appendleft(neighbour)
+
+        if node.value == search_value:
+            return True
+        else:
+            node.visited = True
+
+    return False
+
+if __name__ == '__main__':
+    node0 = Node(0)
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+    node4 = Node(4)
+    node5 = Node(5)
+    node6 = Node(6)
+
+    node0.adjacentNodes = [node1, node2, node3]
+    node1.adjacentNodes = [node5, node6]
+    node2.adjacentNodes = [node4]
+    node3.adjacentNodes = [node2, node4]
+    node4.adjacentNodes = [node1]
+    node5.adjacentNodes = []
+    node6.adjacentNodes = [node4]
+
+    assert breadth_first_search(node0, 4) == True
---
+++ graph-search/node.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.adjacentNodes = []
+        self.visited = False
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return str(self.value) + " " + str(self.visited)
+
+    def __eq__(self, other):
+        return self.value == other.value
