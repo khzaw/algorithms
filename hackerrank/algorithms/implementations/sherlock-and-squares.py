829437b456f61122a7333ee1dc11c8975c8ab8fe
Kaung Htet Zaw <emoosx@gmail.com>
2016-03-21 19:02:13 +0800

    Finish service-lane and sherlock-and-squares

--- hackerrank/algorithms/implementations/sherlock-and-squares.py
+++ hackerrank/algorithms/implementations/sherlock-and-squares.py
@@ -1,9 +1,9 @@
 #!/bin/python
 
-from math import sqrt, floor
+from math import sqrt, floor, ceil
 import sys
 
 t = int(raw_input().strip())
 for a0 in xrange(t):
-    a, b = map(int, raw_input().strip().split(' '))
-    print filter(lambda x: floor(sqrt(x) + 0.5) ** 2 == x, range(a, b+1))
+    a, b = map(sqrt, (map(int, raw_input().strip().split(' '))))
+    print int(floor(b) - ceil(a) + 1)
