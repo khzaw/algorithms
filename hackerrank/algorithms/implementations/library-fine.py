a79f3d6153e1eb547221e1696285241b36198d44
Kaung Htet Zaw <emoosx@gmail.com>
2016-03-21 17:00:37 +0800

    Fixed library fine

--- hackerrank/algorithms/implementations/library-fine.py
+++ hackerrank/algorithms/implementations/library-fine.py
@@ -2,8 +2,6 @@
 
 import sys
 from datetime import date, timedelta
-from calendar import monthrange
-
 
 d1,m1,y1 = raw_input().strip().split(' ')
 d1,m1,y1 = [int(d1),int(m1),int(y1)]
@@ -13,16 +11,6 @@ d2,m2,y2 = raw_input().strip().split(' ')
 d2,m2,y2 = [int(d2),int(m2),int(y2)]
 expected = date(y2, m2, d2)
 
-def monthdelta(d1, d2):
-    delta = 0
-    while True:
-        mdays = monthrange(d1.year, d1.month)[1]
-        d1 += timedelta(days=mdays)
-        if d1 <= d2 and d1.month != d2.month:
-            delta += 1
-        else:
-            break
-    return delta
 
 if actual <= expected:
     print 0
@@ -31,4 +19,4 @@ elif actual.year != expected.year:
 elif actual.month == expected.month:
     print 15 * (actual-expected).days
 elif actual.year == expected.year:
-    print 500 * monthdelta(expected, actual)
+    print 500 * (actual.month - expected.month)
