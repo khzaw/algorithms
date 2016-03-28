cfd27e6f0c15060935deb35f847bcff5e27fe1db
Kaung Htet Zaw <emoosx@gmail.com>
2016-03-28 19:45:35 +0800

    Non optimal answer for manasa-and-stones

--- hackerrank/algorithms/implementations/manasa-and-stones.py
+++ hackerrank/algorithms/implementations/manasa-and-stones.py
@@ -1,8 +1,13 @@
 #!/bin/python
+from itertools import product
 
 t = int(raw_input().strip())
-n = int(raw_input().strip())
-a = int(raw_input().strip())
-b = int(raw_input().strip())
+for a0 in xrange(t):
+  n = int(raw_input().strip())
+  a = int(raw_input().strip())
+  b = int(raw_input().strip())
 
-end = set()
+  end = set()
+  for i in product([a, b], repeat=(n-1)):
+    end.add(reduce(int.__add__, i))
+  print ' '.join(str(x) for x in sorted(end))
