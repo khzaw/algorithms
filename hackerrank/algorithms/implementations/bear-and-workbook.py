09039f29b727d7e9639d09cdc1c1a780e741ce3e
Kaung Htet Zaw <emoosx@gmail.com>
2016-04-02 22:27:43 +0800

    Not done yet

--- hackerrank/algorithms/implementations/bear-and-workbook.py
+++ hackerrank/algorithms/implementations/bear-and-workbook.py
@@ -1,3 +1,23 @@
 #!/bin/python
+from itertools import chain
 
 n,k = map(int, raw_input().strip().split(' '))
+chapters = map(int, raw_input().strip().split(' '))
+#  chapters = chain.from_iterable([c if c <= k else filter(lambda x: x, [k]*(c/k) + [c%k]) for c in chapters])
+
+
+total = 0
+for c in chapters:
+
+
+#  is_special = lambda x, chapter, total: chapter.index(x) + 1 == total
+special = 0
+total = 0
+for c in chapters:
+    chapter = range(1, c+2)
+    pages = [c if c <= k else filter(lambda x: x, [k]*(c/k)+[c%k])]
+    for page in pages:
+        total += 1
+        if is_special(i, chapter, total):
+            special += 1
+print special
