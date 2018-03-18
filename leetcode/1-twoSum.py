d9918bfd04e336dd26cde51aeb9e86922aa1ac93
Kaung Htet Zaw <me@kaunghtet.xyz>
2018-03-19 03:47:41 +0800

    Leetcode

---
+++ leetcode/1-twoSum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        """
+        O(n^2)
+        """
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+
+        """
+        O(n) time and O(n) space
+        """
+        mydict = {}
+        for i, v in enumerate(nums):
+            compliment = target - v
+            if compliment not in mydict:
+                mydict[v] = i
+            else:
+                return [mydict[v], i]
---
+++ leetcode/15-3Sum.py
@@ -0,0 +1,30 @@
+class Solution:
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        solution_list = []
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                if left > i + 1 and nums[left] == nums[left-1]:
+                    left += 1
+                    continue
+                s = nums[i] + nums[left] + nums[right]
+                if s < 0:
+                    left += 1
+                elif s > 0:
+                    right -= 1
+                else:
+                    solution_list.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+
+        return solution_list
