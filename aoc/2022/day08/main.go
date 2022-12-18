ba4e3ac9e96f13e4b487cf1de1ba5b7e5f388d92
Kaung Htet <kayhzaw@gmail.com>
2022-12-19 00:27:55 +0800

    Finish AOC-2022/08

--- aoc/2022/day08/main.go
+++ aoc/2022/day08/main.go
@@ -10,21 +10,19 @@ import (
 var input string
 
 func main() {
-	// forest := parseInput(strings.TrimRight(input, "\n"))
-	forest := [][]int{
-		[]int{3, 0, 3, 7, 3},
-		[]int{2, 5, 5, 1, 2},
-		[]int{6, 5, 3, 3, 2},
-		[]int{3, 3, 5, 4, 9},
-		[]int{3, 5, 3, 9, 0},
-	}
+	forest := parseInput(strings.TrimRight(input, "\n"))
+	// forest := [][]int{
+	// 	[]int{3, 0, 3, 7, 3},
+	// 	[]int{2, 5, 5, 1, 2},
+	// 	[]int{6, 5, 3, 3, 2},
+	// 	[]int{3, 3, 5, 4, 9},
+	// 	[]int{3, 5, 3, 9, 0},
+	// }
 
 	vt, score := IterateForest(forest)
 
 	fmt.Println("Part One: ", vt)
 	fmt.Println("Part Two: ", score)
-
-	fmt.Println(GetScore(forest, 1, 2))
 
 }
 
@@ -55,33 +53,38 @@ func GetScore(forest [][]int, row, col int) int {
 
 	// top
 	for i := row - 1; i >= 0; i-- {
-		if forest[i][col] <= forest[row][col] {
-			top += 1
+		top++
+		if forest[i][col] >= forest[row][col] {
+			break
 		}
 	}
 
 	// bottom
 	for i := row + 1; i < len(forest); i++ {
-		if forest[i][col] <= forest[row][col] {
-			bottom += 1
+		bottom++
+		if forest[i][col] >= forest[row][col] {
+			break
 		}
 	}
 
 	// left
-	for j := col - 1; j >= 0 && forest[row][j] < forest[row][col]; j-- {
-		if forest[row][j] <= forest[row][col] {
-			left++
+	for j := col - 1; j >= 0; j-- {
+		left++
+		if forest[row][j] >= forest[row][col] {
+			break
 		}
 	}
 
 	// right
 	for j := col + 1; j < len(forest[0]); j++ {
-		if forest[row][j] <= forest[row][col] {
-			right++
+		right++
+
+		if forest[row][j] >= forest[row][col] {
+			break
 		}
 	}
 
-	fmt.Println("top", top, "left", left, "right", right, "down", bottom)
+	// fmt.Println("top", top, "left", left, "right", right, "down", bottom)
 
 	return top * bottom * left * right
 }
