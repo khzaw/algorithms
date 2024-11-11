a88592ed899e2aec43a1a8a287a3ccd4ab7a99e9
Kaung Htet <kayhzaw@gmail.com>
2024-11-12 02:26:10 +0800

    Finish NeetCode 1

---
+++ neetcode/neetcode-150/01-duplicate-integer.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        m = dict()
+        for n in nums:
+            if m in m:
+                return True
+            m[n] = True
+        return False
